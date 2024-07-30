1deb15f2c7c0f25d07f70f460f2120843e80d74d
EL-mohands <amrmarei87@gmail.com>
2024-07-29 20:27:46 -0700

    Initial project , and add empty home screen

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routName = 'home_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/home/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routName,
+      routes: {
+        HomeScreen.routName: (context) => HomeScreen(),
+      },
+    );
+  }
+}
+
+
+
